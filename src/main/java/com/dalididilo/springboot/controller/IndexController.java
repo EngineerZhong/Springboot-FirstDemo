264aa126f44192b4659b34f6e30ca5c0625ba8e2
dalididilo <xunwu451@126.com>
2019-11-02 14:45:58 +0800

    完善了一部分工具类，以及作为服务提供给重庆电缆进行请求。

--- src/main/java/com/dalididilo/springboot/controller/IndexController.java
+++ src/main/java/com/dalididilo/springboot/controller/IndexController.java
@@ -1,6 +1,13 @@
 package com.dalididilo.springboot.controller;
 
 
+import com.alibaba.druid.support.json.JSONParser;
+import com.alibaba.druid.support.json.JSONUtils;
+import com.alibaba.fastjson.JSON;
+import com.dalididilo.springboot.bean.ControlDevice;
+import com.dalididilo.springboot.bean.ControlDeviceResult;
+import com.dalididilo.springboot.bean.Result;
+import com.dalididilo.springboot.utils.HttpTool;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
@@ -8,11 +15,14 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class IndexController {
@@ -36,11 +46,48 @@ public class IndexController {
     }
 
     @RequestMapping("/user/toLogin")
-    public ModelAndView login(HttpServletRequest request, HttpServletResponse response){
-        ModelAndView model = new ModelAndView("user/login");
-        model.addObject("ctx", request.getServletContext().getContextPath());
-        return model;
+    public String login(Model model,HttpServletRequest request){
+//        ModelAndView model = new ModelAndView("user/login");
+        model.addAttribute("ctx", request.getServletContext().getContextPath());
+        return "user/login";
     }
+
+
+
+    /**
+     * 控制设备的开关
+     * @param initControlDeviceJson
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/Controller3")
+    public String Controller3(@RequestBody ControlDevice initControlDeviceJson){
+        System.out.println(initControlDeviceJson.toString());
+        String[] result = new String[]{"{\"code\":1,\"info\":\"下发命令成功\"}"
+                ,"{\"code\":0,\"info\":\"当前控制模块已经掉线\"}"
+                ,"{\"code\":-1,\"info\":\"上传字段空值\"}"};
+        int computer=(int)(Math.random()*3);
+
+        System.out.println(result[computer]);
+
+        return result[computer];
+
+//        String requestJson = JSON.toJSONString(initControlDeviceJson); // 请求体。
+//        System.out.println(requestJson);
+//        Result result = new Result(true);
+//        try {
+//            result.setMessage(HttpTool.sendPost(requestJson,"http://112.53.82.25:9000/khcontrol/api/control/ControlDevice",""));
+//            result.setStatusCode(200);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            result.setSuccess(false);
+//            result.setStatusCode(500);
+//            result.setMessage("错误" + e.getMessage());
+//        }
+////        System.out.println(requestJson);
+//        return result;
+    }
+
 
     @RequestMapping("/user/Login")
     public String Login(String name,String password,Model model){
